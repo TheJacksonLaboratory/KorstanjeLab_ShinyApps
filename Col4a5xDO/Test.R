379b1a1826d6159f23aa2c352a17a28336c4d32c
ytakemon <yuka.takemon@gmail.com>
2017-09-05 07:24:17 -0400

    Adding index page

--- Col4a5xDO/Test.R
+++ Col4a5xDO/Test.R
@@ -7,16 +7,36 @@
 # Using R/3.4.1
 # RStudio Server: http://ctshiny01:8787
 # Shiny App Server: http://ctshiny01:3838
+# Images must be in a directory call "www"
 
 # NOTES:
 # Any application that you put in your ~/ShinyApps directory is automatically hosted.
 # The directory must be on helix or ctshiny01.
 # The URL will be in the format  http://ctshiny01:3838/[YOUR USERNAME]/[APP NAME]
+# http://ctshiny01:3838/ytakemon/Shiny_Col4a5xDO
+
+# RESOURCES:
+# layout guide: https://shiny.rstudio.com/articles/layout-guide.html
+
+# DATA TRANSFER FROM CADILLAC TO HELIX
+ssh cadillac
+cd /hpcdata/ytakemon/Col4a5xDO/GBRS_reconstruction/reconstruct
+scp -r best.compiled.genoprob ytakemon@helix.jax.org:/projects/ytakemon/Col4a5xDO
+# prompt will apepar to enter data
+
 
 # LIBRARY INSTALLATION
 # install.packages("devtools")
 # source("http://bioconductor.org/biocLite.R")
 # biocLite("DOQTL")
+# install.packages("ggplot2")
+# install.packages("reshape2")
+
+setwd("/home/ytakemon/ShinyApps/")
 library(ggplot2)
 library(DOQTL)
-setwd("/projects/Shiny_Col4a5xDO")
+library(reshape2)
+library(shiny)
+library(rmakrdown)
+
+runApp("Shiny_Col4a5xDO")
--- Col4a5xDO/app.R
+++ Col4a5xDO/app.R
@@ -1,22 +1,43 @@
+# User Interface --------------------------------------------------------------
 ui <- fluidPage(
-  # App title ----
-  titlePanel("Hello Shiny!"),
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    # Main panel for displaying outputs ----
-    mainPanel(
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+  titlePanel("X-linked Alport Diversity Outbred mouse model – eQTL maps"),
 
+  # Sidebar layout with input and output definitions ------------------
+  sidebarLayout(
+    # Sidebar panel for inputs ------------------
+    sidebarPanel(
+      textInput("Text", label = h3("Gene Query"), value = "Enter gene name or Ensembl ID"),
+      fluidRow(column(3, verbatimTextOutput("value")))
+    ),
+    # Main panel for displaying outputs ------------------
+    mainPanel(
+      imageOutput("eQTL_map")
     )
   )
 )
+
+# Server ----------------------------------------------------------------------
+server <- function(input, output) {
+  output$value <- renderPrint({input$Text})
+
+  image_path <-
+
+}
+
+# Run the app -----------------------------------------------------------------
+shinyApp(ui = ui, server = server)
+
+
+Lrrc74a.eQT
+
+
+
+
+
+
+
+
+
+setwd("/projects/ytakemon/Col4a5xDO/best.compiled.genoprob/plot/RNA_qtl/Complete_eQTL_plots")
+x <- list.files("/projects/ytakemon/Col4a5xDO/best.compiled.genoprob/plot/RNA_qtl/Complete_eQTL_plots")
+x[grep(paste0(".", "Lrrc74a", "."), x)]
