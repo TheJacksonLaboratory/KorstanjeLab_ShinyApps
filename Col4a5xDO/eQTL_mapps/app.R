5d7b317d058de93ec81b998ffab5b16000009204
ytakemon <yuka.takemon@gmail.com>
2017-09-06 11:04:42 -0400

    Now working!

--- Col4a5xDO/eQTL_mapps/app.R
+++ Col4a5xDO/eQTL_mapps/app.R
@@ -3,38 +3,57 @@ library(shiny)
 
 # User Interface --------------------------------------------------------------
 ui <- fluidPage(
+
+  # Title
   titlePanel("Col4a5 x Diversity Outbred – eQTL maps"),
 
   # Sidebar layout with input and output definitions ------------------
   sidebarLayout(
     # Sidebar panel for inputs ------------------
     sidebarPanel(
-        p(span("Please use safari to view eQTL maps!", style = "color:blue")),
-        p("Both Ensembl ID and gene names can be queried."),
-        br(),
-        textInput(inputId = "gene_input", label = "Gene Query", value = "Enter query here", width = "100%"),
-        fluidRow(column(verbatimTextOutput("value"), width = 12))
-        #fluidRow(column(widtextOutput("path_input")))
+      # Header
+      p(span("Please use safari to view eQTL maps!", style = "color:blue")),
+      p("Both Ensembl ID and gene names can be queried."),
+      br(),
+      # Input text box
+      textInput(inputId = "gene_input", label = "Gene Query", value = "Enter query here", width = "100%"),
+      # Confirmation text
+      fluidRow(column(verbatimTextOutput("value"), width = 12)),
+      # Dowload eQTl map
+      downloadButton("download_image", label = "Download")
     ),
+
     # Main panel for displaying outputs ------------------
     mainPanel(
-    imageOutput("image")
+      imageOutput("image")
     )
   )
 )
 # Server ----------------------------------------------------------------------
 server <- function(input, output) {
-  # Display query gene in below text entry box
+
+  # Display query gene in below text entry box -----------------------
   output$value <- renderText({input$gene_input})
 
-  # Render image
+  # Render image -----------------------------
   output$image <- renderImage({
-    # Find image
-    path <- "/home/ytakemon/ShinyApps/Col4a5xDO/eQTL/www/"
-    file <- list.files(path = path, pattern = input$gene_input)
-    filename <- paste0(path,file)
-    list(src = filename)
-    }, deleteFile = FALSE)
+  # Find image
+  path <- "/home/ytakemon/ShinyApps/Col4a5xDO/eQTL/www/"
+  file <- list.files(path = path, pattern = input$gene_input)
+  file_path <- paste0(path,file)
+  list(src = file_path)
+  }, deleteFile = FALSE)
+
+  # Download handler --------------------------
+  output$download_image <- downloadHandler(
+    filename <- paste0(input$gene_input, "_eQTL_map.pdf"),
+    content <- function(downloadFile) {
+      path <- "/home/ytakemon/ShinyApps/Col4a5xDO/eQTL/www/"
+      file <- list.files(path = path, pattern = input$gene_input)
+      file_path <- paste0(path,file)
+      file.copy(file_path, downloadFile)
+    }
+  )
 }
 
 # Run the app -----------------------------------------------------------------
