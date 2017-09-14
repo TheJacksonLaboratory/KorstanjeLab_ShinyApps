5b22f39d98924b41dbe010ccf3f48871d554682c
ytakemon <yuka.takemon@gmail.com>
2017-09-14 10:02:32 -0400

    Rename

---
+++ Col4a5xDO/phenoQTL/app.R
@@ -0,0 +1,84 @@
+library(shiny)
+# All eQTL maps are located in helix @ /home/ytakemon/ShinyApps/Col4a5xDO/phenoQTL/www/
+
+# User Interface --------------------------------------------------------------
+ui <- fluidPage(
+
+  # Title
+  titlePanel("Col4a5 x Diversity Outbred – phenotype QTL maps"),
+
+  # Sidebar layout with input and output definitions ------------------
+  sidebarLayout(
+    # Sidebar panel for inputs ------------------
+    sidebarPanel(
+      # Header
+      p(span("Please use safari to view phenoQTL maps!", style = "color:blue")),
+      br(),
+      # Dropdown menu
+      selectInput(inputId = "pheno",
+                  label = "Select Phenotype",
+                  choices = c("Glomerular filtration rate",
+                              "Albumin normalized to creatinine at 6 weeks",
+                              "Albumin normalized to creatinine at 10 weeks",
+                              "Albumin normalized to creatinine at 15 weeks")),
+      # Dowload eQTl map
+      downloadButton("download_image", label = "Download"),
+
+      br(),
+      br(),
+      div("Col4a5xDO eQTL v.1.0.0, powered by R/Shiny, developed by Yuka Takemon, ",
+          "souce code on ", a("Github", href = "https://github.com/TheJacksonLaboratory/KorstanjeLab_ShinyApps"),
+          " (JAX network only).")
+    ),
+
+    # Main panel for displaying outputs ------------------
+    mainPanel(
+      imageOutput("image")
+    )
+  )
+)
+# Server ----------------------------------------------------------------------
+server <- function(input, output) {
+
+  # Render image -----------------------------
+  output$image <- renderImage({
+    # Find image
+    path <- "/home/ytakemon/ShinyApps/Col4a5xDO/phenoQTL/www/"
+    # Based on input drop down choices
+    if (input$pheno == "Glomerular filtration rate"){
+      file <- "Figure4.1_qtl.log.C2.GFR.noX.pdf"
+    } else if (input$pheno == "Albumin normalized to creatinine at 6 weeks"){
+      file <- "Figure4.2_qtl.log.Alb6wk.noX.pdf"
+    } else if (input$pheno == "Albumin normalized to creatinine at 10 weeks"){
+      file <- "Figure4.3_qtl.log.Alb10wk.noX.pdf"
+    } else if (input$pheno == "Albumin normalized to creatinine at 15 weeks"){
+      file <- "Figure4.4_qtl.log.Alb15wk.noX.pdf"
+    }
+    # Define absolute path and list source
+    file_path <- paste0(path, file)
+    list(src = file_path)
+  }, deleteFile = FALSE)
+
+  # Download handler --------------------------
+  output$download_image <- downloadHandler(
+    filename <- paste0(input$pheno, "_QTL_map.pdf"),
+    content <- function(downloadFile) {
+      path <- "/home/ytakemon/ShinyApps/Col4a5xDO/phenoQTL/www/"
+      # Based on input drop down choices
+      if (input$pheno == "Glomerular filtration rate"){
+        file <- "Figure4.1_qtl.log.C2.GFR.noX.pdf"
+      } else if (input$pheno == "Albumin normalized to creatinine at 6 weeks"){
+        file <- "Figure4.2_qtl.log.Alb6wk.noX.pdf"
+      } else if (input$pheno == "Albumin normalized to creatinine at 10 weeks"){
+        file <- "Figure4.3_qtl.log.Alb10wk.noX.pdf"
+      } else if (input$pheno == "Albumin normalized to creatinine at 15 weeks"){
+        file <- "Figure4.4_qtl.log.Alb15wk.noX.pdf"
+      }
+      file_path <- paste0(path, file)
+      file.copy(file_path, downloadFile)
+    }
+  )
+}
+
+# Run the app -----------------------------------------------------------------
+shinyApp(ui = ui, server = server)
