f92d9e7b744c1635ee4ea234084d761cf9f3c293
ytakemon <yuka.takemon@gmail.com>
2017-09-14 14:59:59 -0400

    Update

--- Col4a5xDO/Isoforms/app.R
+++ Col4a5xDO/Isoforms/app.R
@@ -52,7 +52,7 @@ ui <- fluidPage(
                      label = "Download"),
       br(),
       br(),
-      div("Col4a5xDO eQTL v.1.0.0, powered by R/Shiny, developed by Yuka Takemon, ",
+      div("Col4a5xDO Isoforms v.1.0.0, powered by R/Shiny, developed by Yuka Takemon, ",
           "souce code on ", a("Github", href = "https://github.com/TheJacksonLaboratory/KorstanjeLab_ShinyApps"),
           " (JAX network only).")
     ),
