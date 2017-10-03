5380055c2b0971f90cd01c2f207b69321351c83b
ytakemon <yuka.takemon@gmail.com>
2017-10-03 09:23:05 -0400

    Adding GFRcalc to server

---
+++ GFRcalc/app.R
@@ -0,0 +1,174 @@
+# Condensed server.r and ui.r for Jax website
+library(shiny)
+library(readxl)
+library(ggplot2)
+
+source("/home/ytakemon/ShinyApps/GFRcalc/helpers.R")
+options(stringsAsFactors = FALSE)
+max_plots <- 16
+
+# User Interface --------------------------------------------------------------
+ui <- pageWithSidebar(
+
+  # Application title
+  headerPanel("Glomerular Filtration Rate Estimation"),
+
+  # Sidebar with a slider input for number of observations
+  sidebarPanel(
+    fileInput("GFRfile", "Input file:"),
+    sliderInput("dilution","Dilution:", min=50, max=200, value=100),
+    helpText("Input file should a XLSX file with 4-5 columns and no header:
+              'Animal' (optional), 'Time', 'F1',
+             'F2' and 'F3' (other columns will be ignored)."),
+    checkboxInput("Animal", "Animal (A-Z)", value=TRUE),
+    checkboxInput("MouseId", "Mouse ID", value=TRUE),
+    checkboxInput("Weight", "Weight", value=TRUE),
+    checkboxInput("InjectedVolume", "Injected volume", value=TRUE),
+    checkboxInput("GFR2", "Two-compartment model (2C)", value=TRUE),
+    checkboxInput("GFR4", "2 One-compartment models (2xC1)", value=TRUE),
+    checkboxInput("GFR1", "One-compartment model (1C)", value=TRUE),
+    checkboxInput("GFR3", "Piecewise linear model (PL)", value=TRUE),
+    checkboxInput("Sigma.C2", "Square root of the error variance (Sigma.C2)", value=TRUE),
+    checkboxInput("nNA", "Number of missing observations (nNA)", value=TRUE),
+  #  sliderInput("trhold","Outlier threshold:", min=1, max=10, value=5),
+
+    br(),
+    div("GFRcalc 0.9.5, powered by R/Shiny, developed by Petr Simecek, source code on ",
+        a("Github", href="https://github.com/simecek/GFRcalc"))
+
+  ),
+
+  mainPanel(
+    textOutput("check"),
+    tableOutput('contents'),
+    uiOutput("plots")
+  )
+ )
+# Server ----------------------------------------------------------------------
+server <- function(input, output) {
+
+  # read data file
+  file.upload <- reactive({
+    inFile <- input$GFRfile
+    if (!is.null(inFile)) { # if any file name is given
+      ext <- tools::file_ext(inFile$name)
+      file.rename(inFile$datapath, paste(inFile$datapath, ext, sep=".")) # dirty hack, see issue 85
+
+      output <- read_excel(paste(inFile$datapath, ext, sep="."), sheet=2, col_names = FALSE)
+      output <- output[!is.na(output[,1]),] # remove NA rows
+      if (!is.character(output[,1])) { # if animal IDs missing, add them
+        animalID <- rep(LETTERS, each=length(unique(output[,1])))[1:nrow(output)]
+        output <- cbind(animalID, output)
+      }
+      output <- output[,1:5] # only 5 columns matters
+      names(output) <- c("Animal", "Time", "M1", "M2", "M3")
+
+      # adding information about animals - should be on sheet 3
+      animal.table <- read_excel(paste(inFile$datapath, ext, sep="."), sheet=3, col_names = FALSE)[2:4,-1]
+      attr(output, "animals") <- t(animal.table)
+      output
+    } else {
+      NULL
+    }
+  })
+
+  output$check <- renderText({dt <- file.upload(); check.format(dt)})
+
+  output$contents <- renderTable({
+
+    dt <- file.upload()
+
+    if (!is.null(dt) & check.format(dt)=="") { # everything ok
+
+      # info about animals given? if not add them
+
+      animals <- unique(dt$Animal)
+      animal.table <- attr(dt, "animals")
+      results <- NULL
+
+      for (a in animals) {
+
+        tmp <- subset(dt, Animal==a)
+        tmp <- tmp[order(tmp$Time),]
+
+        # very rough outlier detection
+        tmp[-1,c("M1","M2","M3")][outlier.detect(tmp[-1,], 5)] <- NA
+
+        tmp$mean <- rowMeans(tmp[,c("M1","M2","M3")], na.rm=TRUE)
+        start <- 2 # skip first observation
+        tmp2 <- tmp[start:nrow(tmp),]
+
+        fit1 <- oneexp(y=tmp2$mean,x=tmp2$Time)
+        fit2 <- twoexp(y=tmp2$mean,x=tmp2$Time)
+        fit3 <- linint(y=tmp2$mean,x=tmp2$Time)
+        fit4 <- twoexp.approx(y=tmp2$mean,x=tmp2$Time)
+
+        inj.volume <- as.numeric(animal.table[animals==a,3])
+
+        newrow <- data.frame(Animal = a,
+                             MouseId = animal.table[animals==a,1],
+                             Weight = as.numeric(animal.table[animals==a,2]),
+                             "Injected Volume" = inj.volume,
+
+                             "C2" = input$dilution*inj.volume*tmp$mean[1]*ifelse(is.null(fit2), NA, coef(fit2)[2]*coef(fit2)[4]/(coef(fit2)[1]*coef(fit2)[4]+coef(fit2)[2]*coef(fit2)[3])),
+                             "2xC1" = ifelse(is.null(fit4),NA,input$dilution*inj.volume*tmp$mean[1]/fit4),
+                             "C1" = input$dilution*inj.volume*tmp$mean[1]*ifelse(is.null(fit1), NA, coef(fit1)[2]/coef(fit1)[1]),
+                             "PL" = ifelse(is.null(fit3), NA, input$dilution*inj.volume*tmp$mean[1]/fit3),
+                             "Sigma.C2" = ifelse(is.null(fit2),NA,as.integer(round(summary(fit2)$sigma))),
+                             nNA = sum(is.na(tmp2[,c("M1","M2","M3")])),
+                             check.names = FALSE)
+
+        results <- rbind(results, newrow)
+
+      }
+
+      if (!input$Animal) results$"Animal" <- NULL
+      if (!input$MouseId) results$"MouseId" <- NULL
+      if (!input$Weight) results$"Weight" <- NULL
+      if (!input$InjectedVolume) results$"Injected Volume" <- NULL
+      if (!input$GFR2) results$"C2" <- NULL
+      if (!input$GFR4) results$"2xC1" <- NULL
+      if (!input$GFR1) results$"C1" <- NULL
+      if (!input$GFR3) results$"PL" <- NULL
+      if (!input$Sigma.C2) results$"Sigma.C2" <- NULL
+      if (!input$nNA) results$"nNA" <- NULL
+
+      rownames(results) <- NULL
+
+      return(results)
+
+    } else {
+      return (NULL)
+    }
+  }, digits=1, include.rownames=FALSE)
+
+  output$plots <- renderUI({
+
+    dt <- file.upload()
+
+    if (!is.null(dt) & check.format(dt)=="") { # everything ok
+
+      plot_output_list <- lapply(1:length(unique(dt[,1])), function(i) {
+        plotname <- paste0("plot", i)
+        plotOutput(plotname, height = 390, width = 520)
+      })
+
+      # Convert the list to a tagList - this is necessary for the list of items
+      # to display properly.
+      do.call(tagList, plot_output_list)
+    }
+  })
+
+  for (i in 1:max_plots) {
+    # Need local so that each item gets its own number. Without it, the value
+    # of i in the renderPlot() will be the same across all instances, because
+    # of when the expression is evaluated.
+    local({
+      my_i <- i
+      plotname <- paste0("plot", my_i)
+      output[[plotname]] <- renderPlot({make.plot(file.upload(), my_i)})
+    })
+  }
+}
+# Run the app -----------------------------------------------------------------
+shinyApp(ui = ui, server = server)
---
+++ GFRcalc/helpers.R
@@ -0,0 +1,157 @@
+
+# check file format
+check.format <- function(tb, col.names=c("M1","M2","M3","Time"), min.rows=6) {
+  if (is.null(tb)) return(invisible("")) # no file given
+  if (!is.data.frame(tb)) return("ERROR: Not a data frame.")
+  if (nrow(tb) %% 3 != 0) return("ERROR: Number of rows not divisible by 3.")
+  if (!is.character(tb[,1]))
+    return("ERROR: Animal column must be character.")
+  for (c in 2:5)
+    if (!is.numeric(tb[,c])) return(paste("ERROR: Column",c,"must be numeric."))
+  if (any(is.na(tb[,2]))) return("ERROR: Time must not contain missing values.")
+  if (any(tb[,2]<0 | tb[,2]>200)) return("ERROR: Time must be an integer between 0 and 200.")
+  if (nrow(tb) < min.rows) return(paste("Ar least",min.rows,"rows required."))
+  animal.table <- attr(tb, "animals")
+  if (any(is.na(as.numeric(as.character(animal.table[,3]))))) return("Injected volume must be numeric.")
+  if (nrow(animal.table)!=length(unique(tb[,1]))) return("Number of animals differ between sheets 2 and 3.")
+  return(invisible(""))
+}
+
+# fit log-normal regression
+fit.rlm <- function(y,x) {
+  require(MASS)
+  fit <- lm(log(y)~x)
+  fit
+}
+
+# fit one component model y = A*exp(-B*x) + noise
+oneexp <- function(y,x) {
+  a1 = exp(coef(fit.rlm(y,x))[1])
+  k1 = -coef(fit.rlm(y,x))[2]
+
+  tryCatch(nls(y ~ A*exp(-B*x), start= list(A=a1,B=k1)),
+           error = function(e) NULL)
+}
+
+# fit two components model y = A*exp(-B*x) + C*exp(-D*x) + noise
+twoexp <- function(y,x) {
+
+  stopifnot(length(x) ==length(y))
+  o <- order(x)
+  x <- x[o]
+  y <- y[o]
+  n <- length(x)
+
+  f1 <- oneexp(y[1:4],x[1:4])
+  if (is.null(f1)) return(NULL)
+
+  f2 <- oneexp(y[(n-3):n],x[(n-3):n])
+  if (is.null(f2)) return(NULL)
+
+  a1 <- coef(f1)[1]
+  k1 <- coef(f1)[2]
+  a2 <- coef(f2)[1]
+  k2 <- coef(f2)[2]
+
+  output <- tryCatch(nls(y ~ A*exp(-B*x) + C*exp(-D*x),
+                         start=list(A=a1,B=k1,C=a2, D=k2),
+                         algorithm="port"),
+                     error = function(e) NULL)
+
+  # if not converging, try diffetent optim. algorithm
+  if (is.null(output))
+    output <- tryCatch(nls(y ~ A*exp(-B*x) + C*exp(-D*x),
+                           start=list(A=a1,B=k1,C=a2, D=k2)),
+                       error = function(e) NULL)
+
+  output
+}
+
+# two components approximated by two one component models
+twoexp.approx <- function(y,x) {
+
+  stopifnot(length(x) ==length(y))
+  o <- order(x)
+  x <- x[o]
+  y <- y[o]
+  n <- length(x)
+
+  f1 <- oneexp(y[1:4],x[1:4])
+  if (is.null(f1)) return(NULL)
+
+  f2 <- oneexp(y[(n-3):n],x[(n-3):n])
+  if (is.null(f2)) return(NULL)
+
+  # coefficients
+  a1 <- coef(f1)[1]
+  k1 <- coef(f1)[2]
+  a2 <- coef(f2)[1]
+  k2 <- coef(f2)[2]
+
+  # intersect
+  x0 <- (log(a2) - log(a1))/(k2-k1)
+
+  a1/k1 * (1 - exp(-k1*x0)) + a2/k2 * exp(-k2*x0)
+}
+
+
+# integral of linear approximation from 0 to \int
+linint <- function(y,x) {
+
+  # enforse increasing ordering
+  stopifnot(!any(duplicated(x)))
+  stopifnot(length(x)==length(y))
+  o <- order(x)
+  x <- x[o]
+  y <- y[o]
+
+  # add 0 and \infty
+  n <- length(x)
+  x <- c(0,x,x[n]+(x[n]-x[n-1])/(y[n-1]-y[n])*y[n])
+  y <- c(y[1]+(y[1]-y[2])*x[1]/(x[2]-x[1]),y,0)
+
+  # integration
+  sum(diff(x)*(y[-1]+y[-n])/2)
+}
+
+make.plot <- function(dt, i) {
+  animals <- unique(dt$Animal)
+  a <- animals[i]
+
+  tmp <- subset(dt, Animal == a)
+  tmp <- tmp[order(tmp$Time),]
+
+  tmp$mean <- rowMeans(tmp[,c("M1","M2","M3")], na.rm=TRUE)
+  start <- 2 # skip first observation
+  tmp2 <- tmp[start:nrow(tmp),]
+
+  fit1 <- oneexp(y=tmp2$mean,x=tmp2$Time)
+  fit2 <- twoexp(y=tmp2$mean,x=tmp2$Time)
+
+  if (!is.null(fit2)) {
+    dt.plot <- data.frame(Time = rep(tmp2$Time,4),
+                       Line = rep(c("F1","F2","F3","prediction"), each=nrow(tmp2)),
+                       Fluorescence = c(tmp2$M1, tmp2$M2, tmp2$M3,
+                                        predict(fit2, data.frame(x=tmp2$Time))))
+  } else {
+    medianM = median(c(tmp2$M1, tmp2$M2, tmp2$M3))
+    dt.plot <- data.frame(Time = rep(tmp2$Time,4),
+                          Line = rep(c("F1","F2","F3","prediction"), each=nrow(tmp2)),
+                          Fluorescence = c(tmp2$M1, tmp2$M2, tmp2$M3,
+                                           rep(medianM, nrow(tmp2))))
+  }
+
+  plot(qplot(y=Fluorescence, x=Time, data=dt.plot) +
+         geom_line(aes(group=Line, color=Line)) +
+         ggtitle(a) +
+         theme_bw())
+}
+
+outlier.detect <- function(tmp, trhold) {
+  tmp$mean <- rowMeans(tmp[,c("M1","M2","M3")], na.rm=TRUE)
+  fit <- lm(log2(tmp$mean)~tmp$Time)
+  pred <- 2^predict(fit)
+  output <- abs(log2(tmp[,c("M1","M2","M3")] / pred))>trhold/5
+  return(output)
+
+}
