071bc9e6797fa594f903ee499ed2f7477176de03
ytakemon <yuka.takemon@gmail.com>
2017-10-17 13:12:53 -0400

    Adding location on server

--- PublicJAX/Setup/ctronshiny01_setup.R
+++ PublicJAX/Setup/ctronshiny01_setup.R
@@ -5,3 +5,10 @@ library(c("devtools", "shiny", "ggplot2", "reshape2","readxl", "boot","Matrix","
 library(devtools)
 source("https://bioconductor.org/biocLite.R")
 biocLite(c("qtl","biomaRt","DOQTL"))
+
+
+
+# Shiny run logs are currently located:
+# ytakemon@ctronshiny01:/var/log/shiny-server
+# The apps can be found here:
+# ytakemon@ctronshiny01:/opt/KorstanjeLab
